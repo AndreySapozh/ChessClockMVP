4f4ec740575da4cc93d8c653085968b6fbcfa9b2
SapozhnikovAndreyV <sapozhnikov.andrey.v@yandex.ru>
2023-01-11 11:20:58 +0300

    refactorNewTimeVC

--- ChessClockMVP/NewTimeModule/View/NewTimeViewController.swift
+++ ChessClockMVP/NewTimeModule/View/NewTimeViewController.swift
@@ -22,6 +22,7 @@ class NewTimeViewController: UIViewController {
     
     override func viewDidLoad() {
         
+        navigationItem.title = "Custom Time"
         view.backgroundColor = .white
         configureNameTextField()
         configureTimeLabel(label: timeLabel)
@@ -63,7 +64,7 @@ class NewTimeViewController: UIViewController {
         label.text = " 0:00"
         label.backgroundColor = UIColor(red: 120/255, green: 249/255, blue: 0/255, alpha: 1.0)
         label.font = UIFont.systemFont(ofSize: 20)
-        timeLabelConstraint(label: label)
+        setTimeLabelConstraint(label: label)
     }
     
     private func createNameTextFieldConstraint() {
@@ -74,7 +75,7 @@ class NewTimeViewController: UIViewController {
         nameTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: defaultConstaint * 3).isActive = true
 
         }
-    
+        
     private func createTimeLabelConstraint(label: UILabel) {
         
         view.addSubview(label)
@@ -90,10 +91,10 @@ class NewTimeViewController: UIViewController {
             label.topAnchor.constraint(equalTo: view.topAnchor, constant: defaultConstaint * 5).isActive = true
             label.layer.borderWidth = 0.5
         case incrementLabel:
-            label.topAnchor.constraint(equalTo: view.topAnchor, constant: defaultConstaint * 6).isActive = true
+            label.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 0).isActive = true
             label.layer.borderWidth = 0.5
         case advancedMode:
-            label.topAnchor.constraint(equalTo: view.topAnchor, constant: defaultConstaint * 8).isActive = true
+            label.topAnchor.constraint(equalTo: incrementLabel.bottomAnchor, constant: defaultConstaint).isActive = true
         default:
             return
         }
@@ -111,21 +112,23 @@ class NewTimeViewController: UIViewController {
 
     }
     
-    private func timeLabelConstraint(label: UILabel) {
+    private func setTimeLabelConstraint(label: UILabel) {
         label.layer.masksToBounds = true
         label.layer.cornerRadius = 8
         view.addSubview(label)
         label.translatesAutoresizingMaskIntoConstraints = false
 
-        label.heightAnchor.constraint(equalToConstant: defaultConstaint * 0.75).isActive = true
         label.widthAnchor.constraint(equalToConstant: defaultConstaint * 1.25).isActive = true
-        label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -defaultConstaint * 0.8).isActive = true
         
         switch label {
         case setTimeLabel:
-            label.topAnchor.constraint(equalTo: view.topAnchor, constant: defaultConstaint * 5.125).isActive = true
+            label.topAnchor.constraint(equalTo: timeLabel.topAnchor, constant: defaultConstaint * 0.125).isActive = true
+            label.rightAnchor.constraint(equalTo: timeLabel.rightAnchor, constant: -defaultConstaint * 0.4).isActive = true
+            label.bottomAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: -defaultConstaint * 0.125).isActive = true
         case setIncrementLabel:
-            label.topAnchor.constraint(equalTo: view.topAnchor, constant: defaultConstaint * 6.125).isActive = true
+            label.rightAnchor.constraint(equalTo: incrementLabel.rightAnchor, constant: -defaultConstaint * 0.4).isActive = true
+            label.topAnchor.constraint(equalTo: incrementLabel.topAnchor, constant: defaultConstaint * 0.125).isActive = true
+            label.bottomAnchor.constraint(equalTo: incrementLabel.bottomAnchor, constant: -defaultConstaint * 0.125).isActive = true
         default: return
         }
     }
@@ -137,11 +140,11 @@ class NewTimeViewController: UIViewController {
         switchAdvancedMode.layer.cornerRadius = 8
         
         switchAdvancedMode.translatesAutoresizingMaskIntoConstraints = false
-        switchAdvancedMode.heightAnchor.constraint(equalToConstant: defaultConstaint * 0.75).isActive = true
         switchAdvancedMode.widthAnchor.constraint(equalToConstant: defaultConstaint * 1.25).isActive = true
-        switchAdvancedMode.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -defaultConstaint * 0.8).isActive = true
-        switchAdvancedMode.topAnchor.constraint(equalTo: view.topAnchor, constant: defaultConstaint * 8.125).isActive = true
-       
+        switchAdvancedMode.topAnchor.constraint(equalTo: advancedMode.topAnchor, constant: defaultConstaint * 0.125).isActive = true
+        switchAdvancedMode.rightAnchor.constraint(equalTo: advancedMode.rightAnchor, constant: -defaultConstaint * 0.4).isActive = true
+        switchAdvancedMode.bottomAnchor.constraint(equalTo: advancedMode.bottomAnchor, constant: -defaultConstaint * 0.125).isActive = true
+
     }
 }
 
